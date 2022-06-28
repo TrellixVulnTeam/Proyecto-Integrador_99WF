2699789264eb1cb988ed30e65234b4e535655fa0
Facu Molina <100727508+Facheller@users.noreply.github.com>
2022-06-28 10:39:36 -0300

    Carpeta Backend

---
+++ P-Integrador/BackEnd/src/test/java/com/portafoliofacumoli/Facu/FacuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portafoliofacumoli.Facu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
