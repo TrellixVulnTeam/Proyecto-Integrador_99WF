34280ce005af2049fa93a91eb7cc433efa7cadc8
Facu Molina <facundo.molina14@gmail.com>
2022-07-09 12:31:53 -0300

    Actualizacion?

--- P-Integrador/BackEnd/src/main/java/com/facundoportafolio/Facu/FacuApplication.java
+++ P-Integrador/BackEnd/src/main/java/com/facundoportafolio/Facu/FacuApplication.java
@@ -1,4 +1,4 @@
-package com.portafoliofacumoli.Facu;
+package com.facundoportafolio.Facu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- P-Integrador/BackEnd/src/test/java/com/facundoportafolio/Facu/FacuApplicationTests.java
+++ P-Integrador/BackEnd/src/test/java/com/facundoportafolio/Facu/FacuApplicationTests.java
@@ -1,4 +1,4 @@
-package com.portafoliofacumoli.Facu;
+package com.facundoportafolio.Facu;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
